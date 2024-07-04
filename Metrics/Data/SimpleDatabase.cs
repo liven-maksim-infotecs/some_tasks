713efc7af1f57306e04ba344facea6d48cca2afd
Liven Maksim <Maksim.Liven@infotecs.ru>
2024-07-04 07:51:10 +0300

    feat: seed some data

--- Metrics/Data/SimpleDatabase.cs
+++ Metrics/Data/SimpleDatabase.cs
@@ -2,7 +2,13 @@
 
 public class SimpleDatabase
 {
-    private readonly ConcurrentBag<DeviceStatistic> _dataBase = new();
+    private readonly ConcurrentBag<DeviceStatistic> _dataBase = new()
+    {
+        new(Guid.NewGuid(), "Random Guy", DeviceOSType.Android, "8.800.55.5"),
+        new(Guid.NewGuid(), "The One", DeviceOSType.Windows, "8.800.55.5"),
+        new(Guid.NewGuid(), "Another one", DeviceOSType.Linux, "8.800.44.5"),
+        new(Guid.NewGuid(), "Anonymous", DeviceOSType.MacOs, "8.800.33.8"),
+    };
 
     public IEnumerable<DeviceStatistic> Get() =>
         _dataBase.ToArray();
